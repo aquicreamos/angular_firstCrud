0765907e6e3e947e2dcae2897ef2c38e32c315cb
unknown <Sistemas@Iba_soft_nomin2.alcanos.net>
2019-10-22 14:27:06 -0500

    @bug en form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './user/component/users/users.component';
+import { UserDetailComponent } from './user/component/user-detail/user-detail.component';
+import { UserService } from './core/services/user.service';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClient,
+    HttpClientModule
   ],
-  providers: [],
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClient,
+    HttpClientModule
+  ],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  lastname: number;
+  mobile: string;
+  code: string;
+  email: string;
+  validate: number;
+}
